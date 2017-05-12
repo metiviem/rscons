dab870854a1edcf0fd1492c81f581b44035d2be0
Josh Holtrop <jholtrop@gmail.com>
2017-05-12 13:50:45 -0400

    add Rscons.n_threads

--- lib/rscons.rb
+++ lib/rscons.rb
@@ -41,6 +41,10 @@ module Rscons
 
   class << self
 
+    # @return [Integer]
+    #   The number of threads to use when scheduling subprocesses.
+    attr_accessor :n_threads
+
     # Remove all generated files.
     #
     # @return [void]
@@ -147,7 +151,45 @@ module Rscons
       @command_executer = val
     end
 
+    private
+
+    # Determine the number of threads to use by default.
+    #
+    # @return [Integer]
+    #   The number of threads to use by default.
+    def determine_n_threads
+      # If the user specifies the number of threads in the environment, then
+      # respect that.
+      if ENV["RSCONS_NTHREADS"] =~ /^(\d+)$/
+        return $1.to_i
+      end
+
+      # Otherwise try to figure out how many threads are available on the
+      # host hardware.
+      begin
+        case RbConfig::CONFIG["host_os"]
+        when /linux/
+          return File.read("/proc/cpuinfo").scan(/^processor\s*:/).size
+        when /mswin|mingw/
+          if `wmic cpu get NumberOfLogicalProcessors /value` =~ /NumberOfLogicalProcessors=(\d+)/
+            return $1.to_i
+          end
+        when /darwin/
+          if `sysctl -n hw.ncpu` =~ /(\d+)/
+            return $1.to_i
+          end
+        end
+      rescue
+      end
+
+      # If we can't figure it out, default to 1.
+      1
+    end
+
   end
+
+  @n_threads = determine_n_threads
+
 end
 
 # Unbuffer $stdout
