5fe55a584ea17b15037dbcfd39f4dfe35425295a
Josh Holtrop <jholtrop@gmail.com>
2017-05-17 08:49:53 -0400

    add short description field to ThreadedCommand

--- lib/rscons/threaded_command.rb
+++ lib/rscons/threaded_command.rb
@@ -13,6 +13,11 @@ module Rscons
     #   be passed back into the builder's #finalize method.
     attr_reader :builder_info
 
+    # @return [String]
+    #   Short description of the command. This will be printed to standard
+    #   output if the Environment's echo mode is :short.
+    attr_reader :short_description
+
     # @return [Hash]
     #   Field for Rscons to store the build operation while this threaded
     #   command is executing.
@@ -27,9 +32,13 @@ module Rscons
     # @option options [Object] :builder_info
     #   Arbitrary object to store builder-specific info. This object value will
     #   be passed back into the builder's #finalize method.
+    # @option options [String] :short_description
+    #   Short description of the command. This will be printed to standard
+    #   output if the Environment's echo mode is :short.
     def initialize(command, options = {})
       @command = command
       @builder_info = options[:builder_info]
+      @short_description = options[:short_description]
     end
 
   end
