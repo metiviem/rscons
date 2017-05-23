0d46d616dd289ef3d498e2fa97f72032d0be0847
Josh Holtrop <jholtrop@gmail.com>
2017-05-23 16:09:29 -0400

    change user dependency test to use BuildTarget#depends

--- build_tests/simple/cache_user_dep.rb
+++ build_tests/simple/cache_user_dep.rb
@@ -1,5 +1,5 @@
 Rscons::Environment.new do |env|
-  env.Program("simple.exe", "simple.c")
+  target = env.Program("simple.exe", "simple.c")
   user_deps = File.read("user_deps", mode: "rb").split(" ")
-  env.depends("simple.exe", *user_deps)
+  target.depends(*user_deps)
 end
