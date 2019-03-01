f58744d31f2dd9e213879461147157821b2474bd
Josh Holtrop <jholtrop@gmail.com>
2019-02-28 21:00:39 -0500

    mv build_dist.rb -> rb/build_dist.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -14,7 +14,7 @@ CLEAN.include %w[build_test_run .yardoc doc coverage test]
 CLOBBER.include %w[dist pkg]
 
 task :build_dist do
-  sh "ruby build_dist.rb"
+  sh "ruby rb/build_dist.rb"
 end
 
 RSpec::Core::RakeTask.new(:spec, :example_string) do |task, args|
